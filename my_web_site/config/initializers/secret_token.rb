dde8a1f526fa39b043d73b6e7a7d7d491af0eb58
oualid <oualid@oualid-HP-EliteBook-8530w.(none)>
2013-03-27 12:24:46 +0800

    init

---
+++ my_web_site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWebSite::Application.config.secret_token = '3163c732afcb5c2604873624dff9eb04bdd9d72fb56c233ccd9402e99a43223391b500ff0a9e7cd631a604027fe731f8dd9a5e0e8d494a6f0cab6552e14be733'
